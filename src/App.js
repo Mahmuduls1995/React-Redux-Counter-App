64ee01a384dff4422f0c69470f2b6e92dbf80b1d
mahmudul1995 <mahmuduls451@gmail.com>
2022-07-04 11:59:28 +0600

    done

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import Counter from './components/Counter';
 function App() {
   return (
     <div className="App">
-          <h2>Welcome to the React Redux</h2>
-          <Counter></Counter>
+      <h2>Welcome to the React Redux</h2>
+      <Counter></Counter>
 
     </div>
   );
 }
 
 export default App;
+// create actions & constants
+// create reducers
+// create & provide store
+// useSelector, useDispatch
